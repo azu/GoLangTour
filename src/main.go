24c76cd8a1a9c26652ab598942e796592382acdc
azu <azuciao@gmail.com>
2013-04-14 16:21:06 +0900

    add Numeric Constants

--- src/main.go
+++ src/main.go
@@ -37,14 +37,26 @@ func shortVarFunc() {
 	fmt.Println(x, y, z, c, python, java)
 }
 
+func needInt(x int) int {
+	return x*10 + 1
+}
+func needFloat(x float64) float64 {
+	return x*0.1
+}
+
 func constFunc() {
 	const (
-		String  = "Const が使えFるのは Char,String,Boolean,Number"
+		String  = "Const が使えるのは Char,String,Boolean,Numeric"
 		Pi      = 3.14
 		Boolean = true
+		Big     = 1<<100
+		Small   = Big>>99
 	)
 
 	fmt.Println(Pi, String , Boolean)
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }
 func main() {
 	fmt.Println(add(42, 13))
