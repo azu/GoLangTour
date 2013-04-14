63f31465e75451e1027bd42a6b4d23669abc8dab
azu <azuciao@gmail.com>
2013-04-14 16:13:50 +0900

    add ``varFunc`` , ``shortFunc``

--- src/main.go
+++ src/main.go
@@ -17,9 +17,38 @@ func add(x , y int) int {
 func swap(x, y string) (string, string) {
 	return y, x
 }
+
+func split(sum int) (x, y int) {
+	x = (sum*4)/9
+	y = sum - x
+	return
+}
+
+func varFunc() {
+	var x, y, z int = 1, 2, 3
+	var c, python, java = true, false, "no!!"
+	fmt.Println(x, y, z, c, python, java)
+}
+
+func shortVarFunc() {
+	// in func
+	x, y, z := 1, 2, 3
+	c, python, java := true, false, "no!!"
+	fmt.Println(x, y, z, c, python, java)
+}
+
 func main() {
 	fmt.Println(add(42, 13))
 
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
+
+	// split
+	doubleDigit, singleDigit := split(17)
+	fmt.Println(doubleDigit, singleDigit)
+
+	// varFunc
+	varFunc()
+	// alt shortVarFunc
+	shortVarFunc()
 }
