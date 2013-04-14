a90ad8853aef0775560f70e86768e8933c87ac31
azu <azuciao@gmail.com>
2013-04-14 16:43:01 +0900

    add ``pow`` func

--- src/main.go
+++ src/main.go
@@ -81,6 +81,20 @@ func sqrt(x float64) string {
 	}
 	return fmt.Sprint(math.Sqrt(x))
 }
+
+func pow(x float64, n float64, limit float64) float64 {
+	// let ?
+	if v := math.Pow(x, n); v < limit {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, limit)
+	}
+
+	// ~>
+	// doesn't access ``v`` value
+	return limit
+
+}
 func main() {
 	fmt.Println(add(42, 13))
 
@@ -104,4 +118,12 @@ func main() {
 
 	// sqrt
 	fmt.Println("sqrt: ", sqrt(2), sqrt(-4))
+
+	// pow
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
+
 }
